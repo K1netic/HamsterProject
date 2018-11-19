e2c183b878b89bb55c3f40eef8c07dcc4c959670
Chauveau <martin.chauveau@orange.fr>
2018-11-19 16:41:40 +0100

    Hook
    Tire le projectile
    Détruit quand on relache le bouton

---
+++ HamsterPROJECT/Assets/Scripts/Hook/Hook.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour {
+
+	//AIM
+	public float offset;
+	public GameObject Player;
+	private Vector2 screenPoint;
+
+	//SHOT
+	public GameObject projectile;
+	public Transform shotPoint;
+	public float startTimeBtwShots;
+	private float timeBtwShots;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Player.transform.position;
+		screenPoint.x = (Input.GetAxis("Horizontal"));
+		screenPoint.y = (Input.GetAxis("Vertical"));
+		float rotZ = Mathf.Atan2 (screenPoint.y, screenPoint.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ + offset);
+
+
+		if (Input.GetButtonDown ("Hook")) 
+		{
+			if (timeBtwShots <= 0) 
+				{
+					Instantiate (projectile, shotPoint.position, transform.rotation);
+				}
+				timeBtwShots = startTimeBtwShots;
+				Debug.Log ("Shoot");
+		} 
+
+		else if (Input.GetButtonUp ("Hook")) 
+		{
+			GameObject.Find("hookhead").GetComponent<Projectile>().Destruction();
+			Debug.Log ("Destroy");
+		}
+
+		else
+		{
+			timeBtwShots -= Time.deltaTime;
+
+		}
+		
+	}
+}
---
+++ HamsterPROJECT/Assets/Scripts/Hook/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float speed;
+	private Vector3 direction;
+
+	void Start(){
+		direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		direction = direction.normalized;
+	}
+
+	void Update () {
+		transform.position += direction / speed;
+	}
+
+	public void Destruction(){
+		Destroy (gameObject);
+	}
+}
