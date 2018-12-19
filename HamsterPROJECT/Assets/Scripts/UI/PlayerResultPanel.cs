c90c334c9647fd4525656992027f2d6b62551ce6
Neil Sigfrid <petersflorentin@gmail.com>
2018-12-19 17:46:04 +0100

    Results Screen (WIP)

---
+++ HamsterPROJECT/Assets/Scripts/UI/PlayerResultPanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerResultPanel : MonoBehaviour {
+
+	public int playerResultPanelID;
+	public Image background;
+	Image characterSprite;
+
+	void Awake()
+	{
+		// Determining if the panel must be activated (i.e. the corresponding player was registered)
+		if (GameManager.playersActive [playerResultPanelID - 1])
+			this.gameObject.SetActive (true);
+		else 
+			this.gameObject.SetActive (false);
+
+		characterSprite = this.transform.GetChild (1).GetComponent<Image>();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		background = this.GetComponent<Image> ();
+
+		if (this.gameObject.activeSelf)
+		{
+			characterSprite.sprite = Resources.Load<Sprite> ("CharacterSprites/SelectionScreen/" + GameManager.playersSprites [playerResultPanelID - 1].ToString ());
+		}
+	}
+}
---
+++ HamsterPROJECT/Assets/Scripts/UI/ResultsScreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class ResultsScreen : MonoBehaviour {
+
+	int winnerScore;
+	int winnerIndex;
+	int winnerIndex2;
+	[SerializeField] Color winColor;
+
+	[SerializeField] PlayerResultPanel[] panels;
+
+	void Start()
+	{
+		winnerScore = GameManager.playersScores.Max();
+
+		winnerIndex = System.Array.IndexOf(GameManager.playersScores, winnerScore);
+		winnerIndex2 = System.Array.LastIndexOf(GameManager.playersScores, winnerScore);
+		Debug.Log (winnerIndex.ToString ());
+		Debug.Log (winnerIndex2.ToString ());
+
+		if (winnerIndex != winnerIndex2)
+		{
+			panels [winnerIndex2].background.color = winColor;
+		}
+
+		panels [winnerIndex].background.color = winColor;
+	}
+		
+	void Update()
+	{
+		if (Input.GetButtonDown("Pause_P1"))
+		{
+			SceneManager.LoadScene ("EndGame");
+		}
+	}
+}
