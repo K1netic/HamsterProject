09e2dce11cf377d1eb65ca39d9d2eaa65c11ae57
Chauveau <martin.chauveau@orange.fr>
2018-12-19 16:26:34 +0100

    EXPLOSION.

---
+++ HamsterPROJECT/Assets/Scripts/Environment/Destructible/ExplodeOnClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeOnClick : MonoBehaviour {
+
+	float speed;
+	bool grosZizi;
+
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (!grosZizi) {
+			if (collision.gameObject.CompareTag ("Player")) {
+				print (speed);
+				speed = collision.gameObject.GetComponent<Rigidbody2D> ().velocity.magnitude;
+				if (speed > 10f) {
+					GameObject.FindObjectOfType<ExplosionForce> ().doExplosion (transform.position, speed);
+					grosZizi = true;
+				}
+			}
+		}
+	}
+		
+}
