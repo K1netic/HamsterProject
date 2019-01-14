c6075f8c1792705835aa7f05cd3e77698c5c6b3f
Chauveau <martin.chauveau@orange.fr>
2019-01-14 17:01:35 +0100

    New shields + correctif scroll

--- HamsterPROJECT/Assets/Scripts/ScrollCloud.cs
+++ HamsterPROJECT/Assets/Scripts/ScrollCloud.cs
@@ -6,18 +6,20 @@ public class ScrollCloud : MonoBehaviour {
 
 	[SerializeField]
 	float scrollSpeed = -0.5f;
-
+	float scale;
 	Vector3 startPos;
 
 	// Use this for initialization
 	void Start () {
-		startPos =  new Vector3(transform.position.x, transform.position.y, transform.position.z);
-		print (startPos);
+		startPos = transform.position;
+		scale = transform.parent.GetComponent<Transform>().localScale.x;
+		print (scale);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float nowPos = Mathf.Repeat (Time.time * scrollSpeed, 20);
+		float nowPos = Mathf.Repeat (Time.time * scrollSpeed, 40*scale);
 		transform.position = startPos + Vector3.right * nowPos;
+
 	}
 }
