431183a1b6f7f376f9653a46cc2fe602e2898e59
Ben <ben.peltier7@gmail.com>
2019-02-02 15:14:30 +0100

    UI sprites

--- HamsterPROJECT/Assets/Scripts/Environment/CircularPlatform.cs
+++ HamsterPROJECT/Assets/Scripts/Environment/CircularPlatform.cs
@@ -7,19 +7,32 @@ public class CircularPlatform : MonoBehaviour {
 
 	[SerializeField]
 	GameObject pivot;
-	Vector2 pivotPos;
-	Rigidbody2D rigid;
 	[SerializeField]
 	float moveSpeed;
+    [SerializeField]
+    float rigidBodyMass = 1;
 
+    Rigidbody2D rigid;
+    Vector2 pivotPos;
+    public Vector2 initialVelocity;
+    public float force;
 
     void Start () {
 		pivotPos = pivot.transform.position;
 		rigid = GetComponent<Rigidbody2D>();
-	}
+
+        initialVelocity = moveSpeed * Vector2.Perpendicular(pivotPos - (Vector2)transform.position).normalized;
+
+        force = Mathf.Pow(initialVelocity.magnitude, 2.0f) * Vector2.Distance(pivotPos,(Vector2)transform.position) / rigidBodyMass;
+    }
 	
 	void FixedUpdate () {
-        rigid.velocity = moveSpeed * Vector2.Perpendicular(pivotPos - (Vector2)transform.position).normalized;
+        rigid.AddForce((pivotPos - (Vector2)transform.position).normalized * force);
+
         Debug.DrawRay(transform.position, Vector2.Perpendicular(pivotPos - (Vector2)transform.position).normalized,Color.red,1000);
     }
+
+    
 }
+
+
