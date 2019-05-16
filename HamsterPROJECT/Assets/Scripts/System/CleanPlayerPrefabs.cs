074ddea927c257eed7b94b527a03d20303462c26
Neil Sigfrid <petersflorentin@gmail.com>
2019-05-16 22:48:45 +0200

    little menu modifications

--- HamsterPROJECT/Assets/Scripts/System/CleanPlayerPrefabs.cs
+++ HamsterPROJECT/Assets/Scripts/System/CleanPlayerPrefabs.cs
@@ -19,18 +19,11 @@ public class CleanPlayerPrefabs : MonoBehaviour {
 			Destroy (gameObject);
 		}
 	}
-	string path = "Assets/Resources/Prefabs/TemporaryPrefabs";
 
 	void OnApplicationQuit()
 	{
 		GameObject basePrefab = Resources.Load<GameObject> ("Prefabs/PlayerPrefab");
 		basePrefab.transform.position = new Vector3(0f,0f,0f);
 		basePrefab.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = null;
-
-		if (Directory.Exists(path)) 
-		{ 
-			Directory.Delete(path, true);
-		}
- 		Directory.CreateDirectory(path);
 	}
 }
