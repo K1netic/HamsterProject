5b25bcda8131f6c901b5d731a84120d91d726b78
Neil Sigfrid <petersflorentin@gmail.com>
2019-06-10 18:48:19 +0200

    menus wip

--- HamsterPROJECT/Assets/Scripts/System/LevelSelection.cs
+++ HamsterPROJECT/Assets/Scripts/System/LevelSelection.cs
@@ -31,17 +31,6 @@ public class LevelSelection : MonoBehaviour {
 	string lastLevelPlayed;
 	string levelToLoad;
 
-// 	void Start()
-// 	{
-// 		// Set current pack to base pack
-// 		// currentPack = pack1;
-// //		// Add pack levels to levels pool
-// //		foreach (string level in currentPack)
-// //		{
-// //			levels.Add (level);
-// //		}
-// 	}
-
 	public void SelectPack(int packID)
 	{
 		switch (packID)
@@ -112,39 +101,4 @@ public class LevelSelection : MonoBehaviour {
         }
     }
 
-	/*public void LoadNextLevelFlo(string currentLevel)
-	{
-		//Reset levels to all levels available in pack
-		levels.Clear ();
-		for (int i = 0; i < currentPack.Count; i ++)
-		{
-			levels.Add (currentPack[i]);
-		}
-
-		//Remove last level played from last series of rounds to avoid starting a new series of rounds with the same level
-		if (GameManager.lastLevelPlayed != null)
-		{
-			levels.Remove (GameManager.lastLevelPlayed);
-		}
-
-		//Removing current level played to avoid direct repetition of levels
-		if (currentLevel != null && currentLevel != "")
-		{
-			levels.Remove(currentLevel);
-		}
-
-		//Choosing next level to load from the remaining levels
-		levelToLoad = levels[Random.Range (0, levels.Count)];
-
-		//Loading next scene
-		if (levelToLoad != null)
-		{
-			if (Time.timeScale != 1) Time.timeScale = 1;
-			SceneManager.LoadScene (levelToLoad);
-		}
-		else 
-		{
-			print ("Trying to load next level - no level found");
-		}
-	}*/
 }
